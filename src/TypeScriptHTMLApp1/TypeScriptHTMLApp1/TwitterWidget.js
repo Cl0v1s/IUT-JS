7d63ee942b37268ec54896a566877221eef09180
chaipokoi <chaipokoi@gmail.com>
2016-03-25 21:44:06 +0100

    tentative resoudre twitter

--- src/TypeScriptHTMLApp1/TypeScriptHTMLApp1/TwitterWidget.js
+++ src/TypeScriptHTMLApp1/TypeScriptHTMLApp1/TwitterWidget.js
@@ -18,7 +18,12 @@ var TwitterWidget = (function (_super) {
     TwitterWidget.prototype.showContent = function () {
         var content = document.createElement("div");
         content.innerHTML = '<a class="twitter-timeline"  href="https://twitter.com/MichelBillaud" data-widget-id="712767314249261056">Tweets de @MichelBillaud</a><script></script>';
+        eval('!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ?\'http\' : \'https\'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p +"://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs); } } (document, "script", "twitter-wjs");');
         this.setContent(content);
+    };
+    TwitterWidget.prototype.onUpdate = function () {
+        _super.prototype.onUpdate.call(this);
+        console.log("UPDATE");
         eval('!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ?\'http\' : \'https\'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p +"://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs); } } (document, "script", "twitter-wjs");');
     };
     return TwitterWidget;
--- src/TypeScriptHTMLApp1/TypeScriptHTMLApp1/TwitterWidget.ts
+++ src/TypeScriptHTMLApp1/TypeScriptHTMLApp1/TwitterWidget.ts
@@ -12,7 +12,14 @@
     showContent(): void {
         var content = document.createElement("div");
         content.innerHTML = '<a class="twitter-timeline"  href="https://twitter.com/MichelBillaud" data-widget-id="712767314249261056">Tweets de @MichelBillaud</a><script></script>';
+        eval('!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ?\'http\' : \'https\'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p +"://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs); } } (document, "script", "twitter-wjs");');
+
         this.setContent(content);
+    }
+
+    onUpdate(): void {
+        super.onUpdate();
+        console.log("UPDATE");
         eval('!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ?\'http\' : \'https\'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p +"://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs); } } (document, "script", "twitter-wjs");');
     }
 }
